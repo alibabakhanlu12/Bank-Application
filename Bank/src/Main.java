9667e75e49831dd8d69cb57188bbaf4be2012816
aahmadiv <aliahmadi138102@gmail.com>
2022-03-15 10:59:10 +0330

    iii

--- Bank/src/Main.java
+++ Bank/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Aydin was here!");
-        System.out.println("test main account");
+        System.out.println("test main github account");
     }
 
 }
