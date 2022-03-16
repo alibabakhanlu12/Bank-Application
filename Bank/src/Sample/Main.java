d9cb95763aa5ba32c131b65149e8e5d56532a5b0
Ali Ahmadi <ali.ahmadi.2002d@gmail.com>
2022-03-16 22:01:58 +0330

    adminpage's fxml

--- Bank/src/Sample/Main.java
+++ Bank/src/Sample/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../Customer/AA.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AA.fxml"));
         primaryStage.setTitle("Sample.Main Page");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
